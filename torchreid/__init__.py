3335359495a8bfa25ca43d2ed9162bbb8bffdb5f
hsfzxjy <hsfzxjy@163.com>
2019-03-20 10:49:22 +0800

    First commit.

---
+++ torchreid/__init__.py
@@ -0,0 +1,11 @@
+"""
+deep-person-reid
+==
+
+Description: PyTorch implementation of deep person re-identification models.
+
+Github page: https://github.com/KaiyangZhou/deep-person-reid
+"""
+
+__author__ = 'Kaiyang Zhou'
+__homepage__ = 'https://kaiyangzhou.github.io/'
